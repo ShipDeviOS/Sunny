abab1ba1e5e6a605a7c86b05f733a1f7f9ab1f37
Aleksei Shipilov <shipdev@gmail.com>
2020-09-30 14:36:23 +0300

    finish

---
+++ Supporting files/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Sunny
+//
+//  Created by Aleksei Shipilov on 17.09.2020.
+//  Copyright © 2020 Ivan Akulov. All rights reserved.
+//
+
+import Foundation
+
+let apiKey = "c512c1b7a0e2403aea8d553101c3331a"
